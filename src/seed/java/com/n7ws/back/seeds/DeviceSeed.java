cd169b48ed742263ff148cea8f9ff4f2c380c1ea
TheoBessel <theo.bessel@outlook.com>
2025-05-12 10:04:18 +0200

    feat: Improved seeding for devices

--- src/seed/java/com/n7ws/back/seeds/DeviceSeed.java
+++ src/seed/java/com/n7ws/back/seeds/DeviceSeed.java
@@ -2,6 +2,7 @@ package com.n7ws.back.seeds;
 
 import java.util.ArrayList;
 
+import com.n7ws.back.Random;
 import com.n7ws.back.SeedApplication;
 import com.n7ws.back.entity.DeviceEntity;
 import com.n7ws.back.enums.DeviceState;
@@ -16,9 +17,10 @@ public class DeviceSeed extends Seed {
 
     public void seed() {
         for(int i = 0; i < this.count; i++) {
-            String deviceName = "device" + i;
-            String deviceId = "c" + (i + 200);
-            DeviceEntity device = new DeviceEntity(deviceName, DeviceState.OFFLINE, deviceId, new ArrayList<>(), new ArrayList<>(), new ArrayList<>());
+            String deviceName = Random.randomName();
+            DeviceState deviceState = Random.randomState();
+            String deviceRoom = Random.randomRoom();
+            DeviceEntity device = new DeviceEntity(deviceName, deviceState, deviceRoom, new ArrayList<>(), new ArrayList<>(), new ArrayList<>());
             this.entities.add(device);
         }
     }
---
+++ src/utils/java/com/n7ws/back/Random.java
@@ -0,0 +1,39 @@
+package com.n7ws.back;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+import com.n7ws.back.enums.DeviceState;
+
+public class Random {
+    private static final String ADJECTIVES_FILE = "/Users/theobessel/Documents/N7/2A/AppliWeb/N7WS/back/src/seed/resources/seeding/adjectives.txt";
+    private static final String ANIMAL_NAMES_FILE = "/Users/theobessel/Documents/N7/2A/AppliWeb/N7WS/back/src/seed/resources/seeding/animals.txt";
+
+    public static String randomName() {
+        String[] adjectives;
+        String[] animals;
+        try {
+            adjectives = new String(Files.readAllBytes(Paths.get(ADJECTIVES_FILE))).split("\n");
+            animals = new String(Files.readAllBytes(Paths.get(ANIMAL_NAMES_FILE))).split("\n");
+        } catch (IOException e) {
+            throw new RuntimeException("Error reading files", e);
+        }
+        return adjectives[(int) (Math.random() * adjectives.length)] + "-" +
+               animals[(int) (Math.random() * animals.length)];
+    }
+
+    public static DeviceState randomState() {
+        DeviceState[] states = DeviceState.values();
+        return states[(int) (Math.random() * states.length)];
+    }
+
+    public static String randomRoom() {
+        String[] buildings = {"A", "B", "C", "D", "E", "F", "G", "H", "I", "J"};
+        String[] floors = {"1", "2", "3"};
+        String[] rooms = {"01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20"};
+        return buildings[(int) (Math.random() * buildings.length)] +
+               floors[(int) (Math.random() * floors.length)] +
+               rooms[(int) (Math.random() * rooms.length)];
+    }
+}
